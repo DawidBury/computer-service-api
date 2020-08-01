b013a626039b227da79777083e08fcca30f94267
Dawid <dawidbury1998@gmail.com>
2020-08-01 13:21:18 +0200

    Add validation to user creation

--- src/Controller/UserController.php
+++ src/Controller/UserController.php
@@ -4,16 +4,21 @@ declare(strict_types=1);
 
 namespace App\Controller;
 
+use App\Constraints\CreateUserConstraints;
 use App\Service\UserService;
+use App\Service\ValidatorService;
 use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
 use Symfony\Component\HttpFoundation\JsonResponse;
 use Symfony\Component\HttpFoundation\Request;
 
 class UserController extends AbstractController
 {
-    public function register(Request $request, UserService $userService): JsonResponse
+    public function register(Request $request, UserService $userService, ValidatorService $validatorService): JsonResponse
     {
-        $data = json_decode($request->getContent(), true);
+        $validatorService->validateArray(
+            $data = json_decode($request->getContent(), true),
+            CreateUserConstraints::get()
+        );
 
         $user = $userService->createUser($data['username'], $data['password']);
 
