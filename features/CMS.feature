8f1258e34deb341c4f3b2aff349810519eedc663
Dawid <dawidbury1998@gmail.com>
2020-12-28 19:12:47 +0100

    Fix adding cms entity

--- features/CMS.feature
+++ features/CMS.feature
@@ -1,13 +1,15 @@
 Feature:
+    @login
     Scenario: Try to create CMS with good credentials
         When I add "Content-Type" header equal to "application/json"
-        And I send a "POST" request to "/api/users/register" with body:
+        And I send a "POST" request to "/api/cms" with body:
         """
         {
-            "email": "",
-            "password": "Testowe123!"
+            "attribute": "opening-hours",
+            "value": "8:00"
         }
         """
-        Then the response status code should be 400
+        Then the response status code should be 201
         And the response should be in JSON
-        And the JSON node "[username]" should not be null
+        And the JSON node attribute should not be null
+        And the JSON node value should not be null
--- tests/Behat/FeatureContext.php
+++ tests/Behat/FeatureContext.php
@@ -1,4 +1,6 @@
-<?php declare(strict_types=1);
+<?php
+
+declare(strict_types=1);
 
 namespace App\Tests\Behat;
 
@@ -8,17 +10,20 @@ use Behatch\HttpCall\Request;
 use Behatch\Json\Json;
 use Behatch\Json\JsonInspector;
 use Doctrine\ORM\EntityManagerInterface;
+use Lexik\Bundle\JWTAuthenticationBundle\Services\JWTTokenManagerInterface;
 
 class FeatureContext extends RestContext
 {
     private $inspector;
     private $em;
+    private $jwtManager;
 
-    public function __construct(Request $request, EntityManagerInterface $em)
+    public function __construct(Request $request, EntityManagerInterface $em, JWTTokenManagerInterface $jwtManager)
     {
         parent::__construct($request);
         $this->inspector = new JsonInspector('javascript');
         $this->em = $em;
+        $this->jwtManager = $jwtManager;
     }
 
     /**
@@ -55,5 +60,21 @@ class FeatureContext extends RestContext
     {
         $user = $this->em->getRepository(User::class)->findOneBy(['email' => $email]);
         $this->assertTrue(in_array($role, $user->getRoles()));
+    }
+
+    /**
+     * @BeforeScenario @login
+     */
+    public function login(): void
+    {
+        $user = new User(
+            'test@test.pl'
+        );
+        $user->setPassword('Testowe123!');
+        $this->em->persist($user);
+        $this->em->flush();
+
+        $token = $this->jwtManager->create($user);
+        $this->iAddHeaderEqualTo('Authorization', "Bearer $token");
     }
 }
