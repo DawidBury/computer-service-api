e0009ef3a242fc1c90814382dce4385c6853d62b
Dawid <dawidbury1998@gmail.com>
2021-01-02 15:44:00 +0100

    Add custom response for login

---
+++ src/EventListener/AuthenticationSuccessListener.php
@@ -0,0 +1,43 @@
+<?php
+
+declare(strict_types=1);
+
+namespace App\EventListener;
+
+use App\Entity\User;
+use App\Service\ValidatorService;
+use Doctrine\Common\Annotations\AnnotationReader;
+use Doctrine\ORM\EntityManagerInterface;
+use Lexik\Bundle\JWTAuthenticationBundle\Event\AuthenticationSuccessEvent;
+use Symfony\Component\Serializer\Mapping\Factory\ClassMetadataFactory;
+use Symfony\Component\Serializer\Mapping\Loader\AnnotationLoader;
+use Symfony\Component\Serializer\Normalizer\ObjectNormalizer;
+use Symfony\Component\Serializer\Serializer;
+
+class AuthenticationSuccessListener
+{
+    private $validatorService;
+    private $serializer;
+    private $em;
+
+    public function __construct(ValidatorService $validatorService, EntityManagerInterface $em)
+    {
+        $classMetadataFactory = new ClassMetadataFactory(new AnnotationLoader(new AnnotationReader()));
+        $normalizer = new ObjectNormalizer($classMetadataFactory);
+        $this->serializer = new Serializer([$normalizer]);
+        $this->validatorService = $validatorService;
+        $this->em = $em;
+    }
+
+    public function onAuthenticationSuccessResponse(AuthenticationSuccessEvent $event)
+    {
+        /** @var User $user */
+        $user = $event->getUser();
+
+        $event->setData([
+            'id' => $user->getId(),
+            'email' => $user->getEmail(),
+            'token' => $event->getData()['token'],
+        ]);
+    }
+}
