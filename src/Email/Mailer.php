65f2097cffe742885675b586be4b5b06ab7d15f7
Dawid <dawidbury1998@gmail.com>
2021-01-02 12:54:30 +0100

    Add confirmation email

---
+++ src/Email/Mailer.php
@@ -0,0 +1,35 @@
+<?php
+
+declare(strict_types=1);
+
+namespace App\Email;
+
+use App\Entity\User;
+use Twig\Environment;
+
+class Mailer
+{
+    private $mailer;
+    private $twig;
+
+    public function __construct(\Swift_Mailer $mailer, Environment $twig)
+    {
+        $this->mailer = $mailer;
+        $this->twig = $twig;
+    }
+
+    public function sendConfirmationEmail(User $user)
+    {
+        $body = $this->twig->render(
+            'email/confirmation.html.twig', [
+                'user' => $user,
+            ]
+        );
+        $message = (new \Swift_Message('[computer-service] Potwierdź konto'))
+            ->setFrom('dawidbury1998@gmail.com')
+            ->setTo($user->getEmail())
+            ->setBody($body, 'text/html');
+
+        $this->mailer->send($message);
+    }
+}
