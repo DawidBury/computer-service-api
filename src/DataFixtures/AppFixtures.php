ee228b039581d4eecaf3ba6692ea00491c41c7cd
Dawid <dawidbury1998@gmail.com>
2021-01-03 13:54:20 +0100

    Add fixtures

---
+++ src/DataFixtures/AppFixtures.php
@@ -0,0 +1,34 @@
+<?php
+
+namespace App\DataFixtures;
+
+use Doctrine\Bundle\FixturesBundle\Fixture;
+use Doctrine\Persistence\ObjectManager;
+use Faker\Factory;
+
+abstract class AppFixtures extends Fixture
+{
+    private $manager;
+    private $faker;
+
+    abstract protected function loadData(ObjectManager $manager);
+
+    public function load(ObjectManager $manager)
+    {
+        $this->manager = $manager;
+        $this->faker = Factory::create();
+
+        $this->loadData($manager);
+    }
+
+    protected function createMany(string $className, int $count, callable $factory)
+    {
+        for ($i = 0; $i < $count; ++$i) {
+            $entity = new $className();
+            $factory($entity, $i);
+            $this->manager->persist($entity);
+
+            $this->addReference($className.'_'.$i, $entity);
+        }
+    }
+}
